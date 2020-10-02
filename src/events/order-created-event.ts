72f441f338e924745be0b2a0ed5b93b230884440
emreturgutce <emrebursalee@gmail.com>
2020-10-02 19:16:40 +0300

    typo fixed

--- src/events/order-created-event.ts
+++ src/events/order-created-event.ts
@@ -2,7 +2,7 @@ import { Subjects } from './subjects'
 import { OrderStatus } from './types/order-status'
 
 export interface OrderCreatedEvent {
-  subjects: Subjects.OrderCreated
+  subject: Subjects.OrderCreated
   data: {
     id: string
     status: OrderStatus
